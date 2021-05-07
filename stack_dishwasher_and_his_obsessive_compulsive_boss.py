6f62d283523ef0a2bfb3ef7561af0dc7ed68b16e
MxY <maxianren@gmail.com>
2021-05-07 12:06:34 +0200

    improve dish washer exercise

--- stack_dishwasher_and_his_obsessive_compulsive_boss.py
+++ stack_dishwasher_and_his_obsessive_compulsive_boss.py
@@ -1,7 +1,7 @@
 '''
 Dishwasher John has an obsessive-compulsive boss Mike.
 The restaurant has 10 dishes in total and the boss carefully number them from 0 to 9.
-He asked John to wash the dishes according to the order from 0 to 9, and the washed plates must be neatly stacked.
+He asked John to wash the dishes according to the order from 0 to 9, and the washed_output plates_strt must be neatly stacked.
 
 customers often come to pick up the dishes When John washes the dishes, each customer could only take 1 dish from the top of the dish pile.
 
@@ -14,7 +14,7 @@ Input format:
 A string of length 10, which contains only the numbers from 0 to 9, and does not repeat, representing the number of the plate that the customer took in turn
 
 Output format:
-String: Yes or No, indicating that the dishes are washed in order, or the dishes are not washed in order
+String: Yes or No, indicating that the dishes are washed_output in order, or the dishes are not washed_output in order
 
 Input example 1:
 1043257689
@@ -29,53 +29,32 @@ Output sample 2:
 No
 '''
 
-
-class Stack:
-    def __init__(self):
-        self.items = []
-
-    def isEmpty(self):
-        return self.items == []
-
-    def push(self, item):
-        self.items.append(item)
-
-    def pop(self):
-        return self.items.pop()
-
-    def peek(self):
-        return self.items[len(self.items)-1]
-
-    def size(self):
-        return len(self.items)
-
 def dish_wash_order_check(str):
-    wash_stack=Stack()
-    record_order=str
-    plates=[p for p in range(9,-1,-1)]
-
-    #check each digit of recoded order
-    for r_o in record_order:
-
-
-        if wash_stack.isEmpty():
-
-            while plates!=[] and int(plates[-1])<=int(r_o):
-                wash_stack.push(plates.pop())
-            wash_stack.pop()
-
-        elif int(r_o)==int(wash_stack.peek()):
-            wash_stack.pop()
-
-        else:
-            return 'No'
-
-    return 'Yes'
+    #save the input string into a list
+    input_order=[int(x) for x in str]
+    #simulate the process: start from plates 0-9, washing in the sink and output plates
+    plates_strt=[p for p in range(9,-1,-1)]
+    washed_output, washing_mid = [], []
 
 
+    #main simulation process: check each input number in order
+    for i_o in input_order:
+        #the current number must be the largest among those in the washing sink, thus moving the plates in order from starting place utill the current number
+        while i_o in plates_strt:
+            washing_mid.append(plates_strt.pop())
 
+        #moving plates away from washing sink in order until the current number is transferred
+        while i_o in washing_mid:
+            washed_output.append(washing_mid.pop())
+
+
+    #check if the correct order is equal to the input order
+    if washed_output==input_order:
+        return 'Yes'
+    else:
+        return 'No'
 
 
 if __name__ == "__main__":
-    str=input()
+    str='4230178956'#input()
     print(dish_wash_order_check(str))
